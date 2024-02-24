52571f03e22094898c9deb6f87333d5c5af08820
MAIDENMI <aidenmiah3@gmail.com>
2024-02-24 16:18:31 -0500

    removed components

--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -1,17 +1,20 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Navigation from './components/Navbar';
+import Routes from './Routes';
+
+ReactDOM.render(
+    <Router>
+        <div className="App">
+            <Navigation />
+            <Routes />
+        </div>
+    </Router>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
