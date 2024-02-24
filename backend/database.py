bf9d27ed0654280318a7228c5c9b2947e3aa56da
Anreet Kaur <anreet.kaur@sheridancollege.ca>
2024-02-24 15:27:39 -0500

    [anreet] modified database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,20 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient('localhost', 27017)
+mongo_uri = os.environ.get('MONGO_URI')
+client = MongoClient(mongo_uri)
 db = client.User_Credentials_Database
 user_credentials = db.user_credentials 
 
+def user_insert(self, username, password):
+    user_data ={
+        'username': username,
+        'passowrd': password
+    }
+
+    self.collection.insert_one(user_data)
+
+def find_user(self, username):
+    user_data = self.collection.find_one({'username': username})
+    return user_data
 
